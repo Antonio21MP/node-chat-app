30721942619588021b0bcd1f8cb20bbcf47c0481
Antonio21MP <antonio.mperdomo21@gmail.com>
2018-04-26 14:10:16 -0600

    Greet new user, and alert others.

--- server/server.js
+++ server/server.js
@@ -15,6 +15,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected.');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to chat app.',
+        createAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'new user join.',
+        createAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (msg) => {
         console.log('createMessage', msg);
 
