ac7a64d65ee06f35e609115eed848e03dc12473d
Antonio21MP <antonio.mperdomo21@gmail.com>
2018-04-30 00:09:09 -0600

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -27,24 +27,30 @@ socket.on('newLocationMessage', function(msg) {
 
 jQuery('#message-form').on('submit', function(e){
     e.preventDefault();
+    var msgTextBox = jQuery('[name=message]');
     socket.emit('createMessage', {
         from: 'Jose',
-        text: jQuery('[name=message]').val()
+        text: msgTextBox.val()
     }, function() {
-    
+        msgTextBox.val('');
     });
 });
+
 var locationButton = jQuery('#send-location');
 locationButton.on('click', function(){
     if(!navigator.geolocation){
         return alert('Geolocation not soported by your browser.');
     }
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function(position) {
+        locationButton.removeAttr('disabled').text('Sending location...');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function(){
+        locationButton.removeAttr('disabled').text('Sending location...');
         alert('Unable to fetch location.');
     });
 });
